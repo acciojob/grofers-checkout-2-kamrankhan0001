a20360c5dda54f99873dd89bee0124fc84e36959
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-03 21:27:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your code here
 const prices = document.querySelectorAll('[data-ns-test="prices"]');
-    let total = 0;
-    for (let i = 0; i < prices.length; i++) {
-      total += parseFloat(prices[i].textContent);
-    }
-    const grandTotal = document.createElement('[data-ns-test="grandTotal"]');
-    grandTotal.textContent = total.toFixed(2);
+let totalPrice = 0;
+prices.forEach((price) => {
+  totalPrice += parseFloat(price.innerText);
+});
+
+const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotalCell.innerText = totalPrice.toFixed(2);
