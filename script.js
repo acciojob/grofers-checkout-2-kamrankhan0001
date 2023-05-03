412d34c65e6559969873025b1a78f46ef4db6aea
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-03 21:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-const prices = document.querySelectorAll('[data-ns-test="price"]');
+const prices = document.querySelectorAll('[data-ns-test=price]');
 let totalPrice = 0;
 prices.forEach((price) => {
   totalPrice += parseFloat(price.innerText);
