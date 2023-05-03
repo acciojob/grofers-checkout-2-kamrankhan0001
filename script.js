4d515b2a5f335a8785a8cd241056b4c75ddd871d
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-03 21:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your code here
 const prices = document.querySelectorAll('[data-ns-test=price]');
 let totalPrice = 0;
+//.forEach() method and add up their values using the += operator.
 prices.forEach((price) => {
+//parseFloat() to convert the cell values from strings
+//to floating point numbers
   totalPrice += parseFloat(price.innerText);
 });
 
 const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
-grandTotalCell.innerText = totalPrice.toFixed(2);
+grandTotalCell.innerText = totalPrice.toFixed(2);//to show the price to 2 decimal places.
